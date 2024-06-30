285043bd9de2de30a3429c3c777d0ce735687e1d
JanettheMM <20233tn084@utez.edu.mx>
2024-06-30 15:37:24 -0600

    model-pizza

--- src/main/java/mx/edu/utez/practica/model/Pizza.java
+++ src/main/java/mx/edu/utez/practica/model/Pizza.java
@@ -1,4 +1,54 @@
-package mx.edu.utez.practica.model;
 
-public class Pizza {
+package mx.edu.utez.pizzeria.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Pizza implements Serializable {
+    private int id;
+    private String nombre;
+    private double precio;
+    private ArrayList<Ingrediente> ingredientes;
+
+    public Pizza() {
+    }
+
+    public Pizza(int id, String nombre, double precio, ArrayList<Ingrediente> ingredientes) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.ingredientes = ingredientes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public ArrayList<Ingrediente> getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(ArrayList<Ingrediente> ingredientes) {
+        this.ingredientes = ingredientes;
+    }
 }
